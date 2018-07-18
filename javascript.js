9ce4106e81673b32a6a45abf6ba31c6fdcbe750c
osyed <osyed@snapgroup.co>
2018-07-18 13:01:08 +0300

    add java script

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function handle(){
+	var a = document.getElementById("text-label")
+	a.innerHTML='afetr'
+}
+
+
+// $(document).ready(function(){
+// 	console.log("hi")
+// 	$("button:last").click(function(){
+// 		$("#text-label").html("hihihihi")
+// 	});
+// });
